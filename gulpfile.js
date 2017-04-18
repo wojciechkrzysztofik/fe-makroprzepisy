e8c5b4a113f20c530d6491acc3b2fe8b2f526529
Wojciech Krzysztofik <wkrzysztofik@MacBook-Pro-Wojciech.local>
2017-04-18 16:34:29 +0200

    Gulp improvements.

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,45 @@ var notify = require('gulp-notify');
 var sourcemaps = require('gulp-sourcemaps');
 var injectPartials = require('gulp-inject-partials');
 var webserver = require('gulp-webserver');
+var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var gulpCopy = require('gulp-copy');
 var clean = require('gulp-clean');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
 
 // Run webserver
 gulp.task('webserver', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      port: '8000',
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+  // gulp.src('dist')
+  //   .pipe(webserver({
+  //     port: '8000',
+  //     livereload: true,
+  //     directoryListing: true,
+  //     open: true,
+  //     path: './dist/index.html'
+  //   }))
+
+  connect.server({
+    livereload: true,
+    root: ['.', '.tmp', 'dist']
+  });
 });
+
+// gulp.task('livereload', function() {
+//   gulp.src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
+//     .pipe(watch())
+//     .pipe(connect.reload());
+// });
 
 // inject HTML partials
 gulp.task('html', function () {
   return gulp.src('./src/views/*.html')
            .pipe(injectPartials())
-           .pipe(gulp.dest('./'));
+           .pipe(gulp.dest('./dist/'))
+           .pipe(connect.reload());
 });
 
-// Compile css
+// Compile CSS
 gulp.task('css', function () {
   return gulp.src('src/stylesheets/styles.scss')
     .pipe( sourcemaps.init() )
@@ -40,12 +55,20 @@ gulp.task('css', function () {
         cascade: false
       }))
     .pipe( sourcemaps.write('.') )
-    .pipe( gulp.dest('assets/stylesheets/') )
+    .pipe( gulp.dest('dist/css/') )
     .pipe(notify({
       title: "CSS compiled successfully",
       message: "Jupi!",
       onLast: true
-    }));
+    }))
+    .pipe(connect.reload());
+});
+
+// Minimalize CSS
+gulp.task('minify-css', function() {
+  return gulp.src('dist/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
 
 // Build scripts
@@ -54,12 +77,13 @@ gulp.task('scripts', function () {
       .pipe(sourcemaps.init())
       .pipe(concat('bundle.js'))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('assets/scripts/'))
+      .pipe(gulp.dest('dist/scripts/'))
       .pipe(notify({
           title: "JavaScript builded successfully",
           message: "Jupi!",
           onLast: true
-      }));
+      }))
+      .pipe(connect.reload());
 });
 
 gulp.task('clean', function () {
@@ -67,10 +91,15 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
+gulp.task('copy', function () {
+    gulp.src(['src/fonts/**/*'])
+        .pipe(gulp.dest('dist/fonts/'))
+    gulp.src(['src/images/**/*'])
+        .pipe(gulp.dest('dist/images/'));
+});
+
 gulp.task('build', function () {
-    return gulp
-        .src('src/fonts/**/*')
-        .pipe(gulp.dest('assets/fonts'));
+    gulp.start(['html', 'css', 'minify-css', 'scripts', 'copy']);
 });
 
 // Watch changes
